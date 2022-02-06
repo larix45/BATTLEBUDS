c76d29ca6454362596ffba6c51baa76f97b1c8d5
Akr0nys <55049058+larix45@users.noreply.github.com>
2022-02-06 12:05:07 +0100

    porawki joystick

--- Joystick/Joystick.gd
+++ Joystick/Joystick.gd
@@ -11,11 +11,13 @@ signal moved_raw(direction) # Return raw position of "stick"
 
 export(float) var size = 1.0
 export(float) var stick_escape = 1.2
+export(float) var activation_area = 5.0
 export(Color) var stick_color = Color(0.0, 0.25, 0.6)
 export(Color) var background_color = Color(0.2, 0.2, 0.2, 0.4)
 export(Color) var stick_color_pressed = Color(0.0, 0.15, 0.4)
 export(Color) var background_color_pressed = Color(0.13, 0.13, 0.13, 0.4)
 var offset_pos
+var pressed :bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Galka.color = stick_color
@@ -39,26 +41,22 @@ func _input(event):
 	if self.visible:
 		if event is InputEventScreenTouch:
 			if event.pressed:
-				$Tlo.color = background_color_pressed
-				$Galka.color = stick_color_pressed
-				emit_signal("pressed")
+				var new_pos = event.position - offset_pos
+				if new_pos.x * new_pos.x + new_pos.y * new_pos.y < (100*size*activation_area)*(100*size*activation_area):
+					$Tlo.color = background_color_pressed
+					$Galka.color = stick_color_pressed
+					emit_signal("pressed")
+					pressed = true
 			else:
-				emit_signal("released")
 				$Tlo.color = background_color
 				$Galka.color = stick_color
+				emit_signal("released")
+				pressed = false
 				$Galka.position = Vector2(0,0)
-		if event is InputEventScreenDrag:
+		if event is InputEventScreenDrag and pressed:
 			emit_signal("moved", $Galka.position.normalized())
 			emit_signal("moved_raw", $Galka.position)
 			var new_pos = event.position - offset_pos
-#			if new_pos.x > 100*size:
-#				new_pos.x = 100*size
-#			if new_pos.x < -100*size:
-#				new_pos.x = -100*size
-#			if new_pos.y > 100*size:
-#				new_pos.y = 100*size
-#			if new_pos.y < -100*size:
-#				new_pos.y = -100*size
 #			In general, x and y must satisfy (x - center_x)² + (y - center_y)² < radius².
 			if not new_pos.x * new_pos.x + new_pos.y * new_pos.y < (100*size*stick_escape)*(100*size*stick_escape):
 				new_pos = new_pos.normalized() * 100 * size * stick_escape
